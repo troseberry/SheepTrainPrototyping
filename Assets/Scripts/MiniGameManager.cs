4489cad0aafa1d05ecdc604f0086b5972283ac41
troseberry <tayroseberry@gmail.com>
2017-06-15 05:18:56 -0400

    Finished SpeedLevers Prototype

---
+++ Assets/Scripts/MiniGameManager.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MiniGameManager : MonoBehaviour 
+{
+    public static MiniGameManager ManagerReference;
+
+    bool inGame;
+
+	public float timer;
+	public Text taskTimerText;
+	String formattedTimer;
+
+    public bool didWin;
+
+    public GameObject generalElements;
+    public GameObject passText;
+    public GameObject failText;
+
+    public GameObject speedLevers;
+
+	void Start () 
+	{
+        ManagerReference = this;
+        inGame = false;
+        didWin = false;
+
+        //taskTimerText = transform.Find("TaskTimer").GetComponent<Text>();
+        timer = 5f;
+	}
+	
+	void Update () 
+	{
+        if (inGame)
+        {
+            if (timer > 0)
+            {
+                timer -= Time.deltaTime;
+            }
+            else
+            {
+                timer = 0;
+            }
+            formattedTimer = string.Format("{0:00.00}", timer % 60);
+            taskTimerText.text = formattedTimer;
+        }
+
+		DebugPanel.Log("Timer: ", timer);
+	}
+
+    public void OpenMiniGame ()
+    {
+        //uiEventSystem.currentSelectedGameObject.name;
+    }
+
+    void StartGeneralTasks()
+    {
+        generalElements.SetActive(true);
+        timer = 5.0f;
+    }
+
+    public void EndMiniGame()
+    {
+        inGame = false;
+        if (didWin)
+        {
+            passText.SetActive(true);
+        }
+        else
+        {
+            failText.SetActive(true);
+        }
+        Invoke("CloseMiniGame", 1.5f);
+    }
+
+    public void CloseMiniGame()
+    {
+        generalElements.SetActive(false);
+        passText.SetActive(false);
+        failText.SetActive(false);
+
+        SpeedLevers.SpeedLeversReference.ResetGame();
+    }
+
+    public void OpenSpeedMiniGame()
+    {
+        StartGeneralTasks();
+        
+        speedLevers.SetActive(true);
+        inGame = true;
+    }
+
+    
+}
--- Assets/Scripts/SpeedLevers.cs
+++ Assets/Scripts/SpeedLevers.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 
 public class SpeedLevers : MonoBehaviour 
 {
+    public static SpeedLevers SpeedLeversReference;
+
 	int matchPos_01 = 0;
 	int matchPos_02 = 0;
 	int matchPos_03 = 0;
@@ -23,17 +25,16 @@ public class SpeedLevers : MonoBehaviour
 	bool leverTwoDone;
 	bool leverThreeDone;
 
-	void Awake()
-	{
-		//pick 3 random lever positions
-		matchPos_01 = (int) Mathf.Round(Random.Range(0f, 1f));
-		matchPos_02 = (int) Mathf.Round(Random.Range(0f, 1f));
-		matchPos_03 = (int) Mathf.Round(Random.Range(0f, 1f));
-	}
-
 	void Start () 
 	{
-		matchText_01 = transform.Find("MatchPositions/Position_01");
+        SpeedLeversReference = this;
+
+        //pick 3 random lever positions
+        matchPos_01 = (int)Mathf.Round(Random.Range(0f, 1f));
+        matchPos_02 = (int)Mathf.Round(Random.Range(0f, 1f));
+        matchPos_03 = (int)Mathf.Round(Random.Range(0f, 1f));
+
+        matchText_01 = transform.Find("MatchPositions/Position_01");
 		matchText_01.GetChild(matchPos_01).GetComponent<Text>().enabled = true;
 
 		matchText_02 = transform.Find("MatchPositions/Position_02");
@@ -64,5 +65,45 @@ public class SpeedLevers : MonoBehaviour
 		DebugPanel.Log("Lever 03 Done: ", leverThreeDone);
 
 		DebugPanel.Log("Game Complete: ", isComplete);
+
+        if (isComplete || MiniGameManager.ManagerReference.timer == 0f) CloseGame();
 	}
+
+    void CloseGame()
+    {
+        matchText_01.GetChild(matchPos_01).GetComponent<Text>().enabled = false;
+        matchText_02.GetChild(matchPos_02).GetComponent<Text>().enabled = false;
+        matchText_03.GetChild(matchPos_03).GetComponent<Text>().enabled = false;
+
+        MiniGameManager.ManagerReference.didWin = isComplete;
+        MiniGameManager.ManagerReference.EndMiniGame();
+        gameObject.SetActive(false);
+        //Invoke("CloseGameOverlay", 0.5f);
+    }
+
+    //void CloseGameOverlay()
+    //{
+    //    gameObject.SetActive(false);
+    //}
+
+    public void ResetGame ()
+    {
+        isComplete = false;
+        matchPos_01 = (int)Mathf.Round(Random.Range(0f, 1f));
+        matchPos_02 = (int)Mathf.Round(Random.Range(0f, 1f));
+        matchPos_03 = (int)Mathf.Round(Random.Range(0f, 1f));
+
+        lever_01.value = 0;
+        lever_02.value = 0;
+        lever_03.value = 0;
+
+        leverOneDone = false;
+        leverTwoDone = false;
+        leverThreeDone = false;
+
+        matchText_01.GetChild(matchPos_01).GetComponent<Text>().enabled = true;
+        matchText_02.GetChild(matchPos_02).GetComponent<Text>().enabled = true;
+        matchText_03.GetChild(matchPos_03).GetComponent<Text>().enabled = true;
+
+    }
 }
