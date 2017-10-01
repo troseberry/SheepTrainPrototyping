77a89bcbfad4d5a31b64578bf1dbfba507224be0
troseberry <taylorroseberry@Taylors-MacBook-Pro.local>
2017-10-01 12:15:08 -0700

    Added Boiler Car with sprites

---
+++ Assets/Scripts/BackgroundRepeater.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundRepeater : MonoBehaviour 
+{
+	private BoxCollider2D backgroundCollider;
+	private float backgroundHorizontalLength;
+
+
+	void Awake()
+	{
+		backgroundCollider = GetComponent<BoxCollider2D>();
+		backgroundHorizontalLength = backgroundCollider.size.x;
+	}
+
+	void Update() 
+	{
+		if (transform.position.x > backgroundHorizontalLength)
+		{
+			RepositionBackground();
+		}
+	}
+
+	void RepositionBackground()
+	{
+		Vector2 backgroundOffset = new Vector2(-backgroundHorizontalLength, 0);
+
+		transform.position = backgroundOffset;
+	}
+}
---
+++ Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour 
+{
+	public float scrollSpeed;	
+
+	private Rigidbody2D backgroundRb;
+
+	void Start()
+	{
+		backgroundRb = GetComponent<Rigidbody2D>();
+		backgroundRb.velocity = new Vector2(scrollSpeed, 0);
+	}
+
+	void Update () 
+	{
+		
+	}
+}
