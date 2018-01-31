8ba89e720954aeddf2e29601dcc170d97102dfaa
troseberry <taylorroseberry@Taylors-MacBook-Pro.local>
2018-01-31 00:36:47 -0800

    WakeGuests: shake check is more consistent
    - made logic to check for a shake more consistent
    - cleaned up WakeGuets.cs: shortened some long methods/redundant code

--- Assets/Scripts/WakeGuests.cs
+++ Assets/Scripts/WakeGuests.cs
@@ -8,53 +8,47 @@ public class WakeGuests : MiniGameScript
 	public static WakeGuests WakeGuestsReference;
 	public static float timeLimit = 7.5f;
 
-	float previousDepthAccel = 0;
-	float currentDepthAccel = 0;
-	float deltaDepthAccel = 0;
-	int depthShakeCount = 0;
+	private Vector3 shakeForce;
+	private bool didShake;
+	private bool didHoldStill;
+	private float shakeThreshold = 3.0f;
+	private float stillThreshold = 1.25f;
 
 	public Transform guestGroup;
 
-	Transform guest_01;
-	Transform guest_02;
-	Transform guest_03;
-	Transform guest_04;
-	Transform guest_05;
+	private Transform[] guestTransforms;
 
-	int currentGuest = 0;
-	int guest01Rand;
-	int guest02Rand;
-	int guest03Rand;
-	int guest04Rand;
-	int guest05Rand;
+	private int currentGuest = 0;
+	private int[] randGuestTimes = new int[5];
 
-	bool doStartGame = true;
+	private bool doStartGame = true;
 	public bool failedEarly = false;
 
-	bool doLerpGuestGroup = false;
+	private bool doLerpGuestGroup = false;
 	public float moveDuration;
-	float currentMoveTime;
-	Vector3 initialHorzPos;
-	Vector3 nextHorzPos;
+	private float currentMoveTime;
+	private Vector3 initialHorzPos;
+	private Vector3 nextHorzPos;
 
 
 	void Start () 
 	{
 		WakeGuestsReference = this;
 
-		guest_01 = guestGroup.GetChild(0);
-		guest_02 = guestGroup.GetChild(1);
-		guest_03 = guestGroup.GetChild(2);
-		guest_04 = guestGroup.GetChild(3);
-		guest_05 = guestGroup.GetChild(4);
+		guestTransforms = new Transform[]
+		{
+			guestGroup.GetChild(0),
+			guestGroup.GetChild(1),
+			guestGroup.GetChild(2),
+			guestGroup.GetChild(3),
+			guestGroup.GetChild(4)
+		};
 
 		currentMoveTime = 0;
 	}
 	
 	void Update () 
 	{
-		UpdateDepthShakeCount();
-
 		if (MiniGameManager.IsInGame() && doStartGame)
 		{
 			RandomizeGuestTimes();
@@ -78,6 +72,9 @@ public class WakeGuests : MiniGameScript
 				doLerpGuestGroup = false;
 			}
 		}
+
+		CalculateShakeForce();
+		Debug.Log("Force: " + shakeForce.sqrMagnitude);
 	}
 
 	IEnumerator StartGuestCycle()
@@ -90,150 +87,58 @@ public class WakeGuests : MiniGameScript
 
 	IEnumerator CycleGuestGroup()
 	{
-		yield return new WaitForSeconds(0.25f);
+
+		for (int i = 0; i < 5; i++)
+		{
+			yield return new WaitForSeconds(0.25f);
 		
-		initialHorzPos = guestGroup.localPosition;
-		nextHorzPos = new Vector3(-1475, 0, 0);
-		currentGuest = 1;
-		doLerpGuestGroup = true;
+			initialHorzPos = guestGroup.localPosition;
+			nextHorzPos = new Vector3((-1475 * (i + 1)), 0, 0);
+			currentGuest = (i + 1);
+			doLerpGuestGroup = true;
 
-		yield return new WaitForSeconds(1.25f);
-		CheckFailure();
-		if (failedEarly)
-		{
-			guest_01.GetChild(3).GetComponent<RawImage>().enabled = true;
-		}
-		else
-		{
-			guest_01.GetChild(2).GetComponent<RawImage>().enabled = true;
-		}
-		yield return new WaitForSeconds(0.25f);
-
-		initialHorzPos = guestGroup.localPosition;
-		nextHorzPos = new Vector3(-2950, 0, 0);
-		currentGuest = 2;
-		doLerpGuestGroup = true;
-
-		yield return new WaitForSeconds(1.25f);
-		CheckFailure();
-		if (failedEarly)
-		{
-			guest_02.GetChild(3).GetComponent<RawImage>().enabled = true;
-		}
-		else
-		{
-			guest_02.GetChild(2).GetComponent<RawImage>().enabled = true;
-		}
-		yield return new WaitForSeconds(0.25f);
-		
-		initialHorzPos = guestGroup.localPosition;
-		nextHorzPos = new Vector3(-4425, 0, 0);
-		currentGuest = 3;
-		doLerpGuestGroup = true;
-
-		yield return new WaitForSeconds(1.25f);
-		CheckFailure();
-		if (failedEarly)
-		{
-			guest_03.GetChild(3).GetComponent<RawImage>().enabled = true;
-		}
-		else
-		{
-			guest_03.GetChild(2).GetComponent<RawImage>().enabled = true;
-		}
-		yield return new WaitForSeconds(0.25f);
-
-		
-		initialHorzPos = guestGroup.localPosition;
-		nextHorzPos = new Vector3(-5900, 0, 0);
-		currentGuest = 4;
-		doLerpGuestGroup = true;
-
-		yield return new WaitForSeconds(1.25f);
-		CheckFailure();
-		if (failedEarly)
-		{
-			guest_04.GetChild(3).GetComponent<RawImage>().enabled = true;
-		}
-		else
-		{
-			guest_04.GetChild(2).GetComponent<RawImage>().enabled = true;
-		}
-		yield return new WaitForSeconds(0.25f);
-		
-		initialHorzPos = guestGroup.localPosition;
-		nextHorzPos = new Vector3(-7375, 0, 0);
-		currentGuest = 5;
-		doLerpGuestGroup = true;
-
-		yield return new WaitForSeconds(1.5f);
-		CheckFailure();
-		if (failedEarly)
-		{
-			guest_05.GetChild(3).GetComponent<RawImage>().enabled = true;
-		}
-		else
-		{
-			guest_05.GetChild(2).GetComponent<RawImage>().enabled = true;
+			yield return new WaitForSeconds(1.25f);
+			
+			failedEarly = DetermineShakeResult(randGuestTimes[currentGuest - 1]);
+			if (failedEarly)
+			{
+				guestTransforms[i].GetChild(3).GetComponent<RawImage>().enabled = true;
+			}
+			else
+			{
+				guestTransforms[i].GetChild(2).GetComponent<RawImage>().enabled = true;
+			}
 		}
 		yield return new WaitForSeconds(0.25f);
 	}
 
 	void RandomizeGuestTimes()
 	{
-		guest01Rand = (int)Mathf.Round(Random.Range(0f, 1f));
-		guest_01.GetChild(guest01Rand).GetComponent<RawImage>().enabled = true;
-
-		guest02Rand = (int)Mathf.Round(Random.Range(0f, 1f));
-		guest_02.GetChild(guest02Rand).GetComponent<RawImage>().enabled = true;
-
-		guest03Rand = (int)Mathf.Round(Random.Range(0f, 1f));
-		guest_03.GetChild(guest03Rand).GetComponent<RawImage>().enabled = true;
-
-		guest04Rand = (int)Mathf.Round(Random.Range(0f, 1f));
-		guest_04.GetChild(guest04Rand).GetComponent<RawImage>().enabled = true;
-
-		guest05Rand = (int)Mathf.Round(Random.Range(0f, 1f));
-		guest_05.GetChild(guest05Rand).GetComponent<RawImage>().enabled = true;
-	}
-
-
-	void CheckFailure()
-	{
-		if (currentGuest == 1)
+		for (int i = 0; i < 5; i++)
 		{
-			failedEarly = (guest01Rand == 0) ? !DepthShakeDetected() : DepthShakeDetected();	
-		}
-		else if (currentGuest == 2)
-		{
-			failedEarly = (guest02Rand == 0) ? !DepthShakeDetected() : DepthShakeDetected();
-		}
-		else if (currentGuest == 3)
-		{
-			failedEarly = (guest03Rand == 0) ? !DepthShakeDetected() : DepthShakeDetected();
-		}
-		else if (currentGuest == 4)
-		{
-			failedEarly = (guest04Rand == 0) ? !DepthShakeDetected() : DepthShakeDetected();
-		}
-		else if (currentGuest == 5)
-		{
-			failedEarly = (guest05Rand == 0) ? !DepthShakeDetected() : DepthShakeDetected();
+			randGuestTimes[i] = (int)Mathf.Round(Random.Range(0f, 1f));
+			guestTransforms[i].GetChild(randGuestTimes[i]).GetComponent<RawImage>().enabled = true;
 		}
 	}
 
-	void UpdateDepthShakeCount()
+	void CalculateShakeForce()
 	{
-		previousDepthAccel = currentDepthAccel;
-		currentDepthAccel = Input.acceleration.z;
-		deltaDepthAccel = currentDepthAccel - previousDepthAccel;
-		if (Mathf.Abs(deltaDepthAccel) > 0.5f) depthShakeCount++;
+		shakeForce = Input.acceleration;
+
+		didShake = (shakeForce.sqrMagnitude >= shakeThreshold);
+		didHoldStill = (shakeForce.sqrMagnitude <= stillThreshold);
 	}
 	
-	bool DepthShakeDetected()
+	bool DetermineShakeResult(int guestTime)
 	{
-		bool result = depthShakeCount > 10;
-		depthShakeCount = 0;
+		bool result = false;
+
+		if (guestTime == 0) result = didHoldStill;
+		else if (guestTime == 1) result = didShake;
+
+		didShake = false;
+		didHoldStill = false;
+
 		return result;
 	}
 
@@ -248,38 +153,20 @@ public class WakeGuests : MiniGameScript
 	public override void ResetGame()
 	{
 		currentMoveTime = 0;
+		currentGuest = 0;
+		
+		guestGroup.localPosition = new Vector3(0,0,0);
+
+		for (int i = 0; i < 5; i++)
+		{
+			guestTransforms[i].GetChild(0).GetComponent<RawImage>().enabled = false;
+			guestTransforms[i].GetChild(1).GetComponent<RawImage>().enabled = false;
+			guestTransforms[i].GetChild(2).GetComponent<RawImage>().enabled = false;
+			guestTransforms[i].GetChild(3).GetComponent<RawImage>().enabled = false;
+		}
 		
 		doStartGame = true;
 		failedEarly = false;
-
-		currentGuest = 0;
-
-		guestGroup.localPosition = new Vector3(0,0,0);
-
-		guest_01.GetChild(0).GetComponent<RawImage>().enabled = false;
-		guest_01.GetChild(1).GetComponent<RawImage>().enabled = false;
-		guest_01.GetChild(2).GetComponent<RawImage>().enabled = false;
-		guest_01.GetChild(3).GetComponent<RawImage>().enabled = false;
-
-		guest_02.GetChild(0).GetComponent<RawImage>().enabled = false;
-		guest_02.GetChild(1).GetComponent<RawImage>().enabled = false;
-		guest_02.GetChild(2).GetComponent<RawImage>().enabled = false;
-		guest_02.GetChild(3).GetComponent<RawImage>().enabled = false;
-
-		guest_03.GetChild(0).GetComponent<RawImage>().enabled = false;
-		guest_03.GetChild(1).GetComponent<RawImage>().enabled = false;
-		guest_03.GetChild(2).GetComponent<RawImage>().enabled = false;
-		guest_03.GetChild(3).GetComponent<RawImage>().enabled = false;
-
-		guest_04.GetChild(0).GetComponent<RawImage>().enabled = false;
-		guest_04.GetChild(1).GetComponent<RawImage>().enabled = false;
-		guest_04.GetChild(2).GetComponent<RawImage>().enabled = false;
-		guest_04.GetChild(3).GetComponent<RawImage>().enabled = false;
-
-		guest_05.GetChild(0).GetComponent<RawImage>().enabled = false;
-		guest_05.GetChild(1).GetComponent<RawImage>().enabled = false;
-		guest_05.GetChild(2).GetComponent<RawImage>().enabled = false;
-		guest_05.GetChild(3).GetComponent<RawImage>().enabled = false;
 	}
 
 	public override float GetTimeLimit() { return timeLimit; }
