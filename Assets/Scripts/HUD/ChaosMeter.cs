a5209814cf32af0f761a472963e9aaf474010ce1
troseberry <tayroseberry@gmail.com>
2018-07-10 03:00:33 -0400

    Fixed issue where HUD appeared over mini games UI
    - moved the HUD elements to their own canvas and changed the canvas to
    screen space - camera with a sorting layer behind the mini game UI layer

--- Assets/Scripts/HUD/ChaosMeter.cs
+++ Assets/Scripts/HUD/ChaosMeter.cs
@@ -25,27 +25,28 @@ public class ChaosMeter : MonoBehaviour
 		// DebugPanel.Log("Meter Value: ", meterValue);
 		// DebugPanel.Log(" Offset: ", fillMaskTransform.offsetMax);
 
-		if (RoundManager.GetRoundTimer() > 0)
+		if (NetworkedChaosManager.ChaosManagerReference)
 		{
-			currentMeterValue = Mathf.Ceil(Mathf.Abs(fillMaskTransform.offsetMax.x));
-			nextMeterValue = (float) (600 -(NetworkedChaosManager.ChaosManagerReference.GetChaosValue() * 6));
-
-			if (nextMeterValue != currentMeterValue)
+			if (RoundManager.GetRoundTimer() > 0)
 			{
-				meterLerpValue = Mathf.Lerp(currentMeterValue, nextMeterValue, Time.deltaTime * 15);
+				currentMeterValue = Mathf.Ceil(Mathf.Abs(fillMaskTransform.offsetMax.x));
+				nextMeterValue = (float) (600 -(NetworkedChaosManager.ChaosManagerReference.GetChaosValue() * 6));
 
-				fillMaskTransform.offsetMax = new Vector2(-meterLerpValue,fillMaskTransform.offsetMax.y );
-
-				if (meterLerpValue <= (nextMeterValue * 1.01))
+				if (nextMeterValue != currentMeterValue)
 				{
-					// Close enough. lerp won't get exactly to value unless it has more
-					// time. 15x speed on the lerp isn't enough. but faster doesn't
-					// show a lot of movement
-					fillMaskTransform.offsetMax = new Vector2(-nextMeterValue,fillMaskTransform.offsetMax.y );
+					meterLerpValue = Mathf.Lerp(currentMeterValue, nextMeterValue, Time.deltaTime * 15);
+
+					fillMaskTransform.offsetMax = new Vector2(-meterLerpValue,fillMaskTransform.offsetMax.y );
+
+					if (meterLerpValue <= (nextMeterValue * 1.01))
+					{
+						// Close enough. lerp won't get exactly to value unless it has more
+						// time. 15x speed on the lerp isn't enough. but faster doesn't
+						// show a lot of movement
+						fillMaskTransform.offsetMax = new Vector2(-nextMeterValue,fillMaskTransform.offsetMax.y );
+					}
 				}
 			}
-
-			
 		}
 	}
 
